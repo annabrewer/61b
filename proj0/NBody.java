58b7ba8b3389706447c88c4940a738af1c0dcb2a
Anna  Brewer <Anna@calvisitor-10-105-142-114.calvisitor.1918.berkeley.edu>
2017-01-27 15:53:20 -0800

    Completed project 0

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,3 +1,63 @@
-public class NBody {
+import java.io.*;
+import java.util.*;
 
-}
+public class NBody {	
+
+	public static void main(String[] args) {
+		double t = Double.parseDouble(args[0]);
+		double dT = Double.parseDouble(args[1]);
+		String filename = args[2];
+		double radius = readRadius(filename);
+		Planet[] planets= readPlanets(filename);
+		StdDraw.setXscale(-radius, radius);
+		StdDraw.setYscale(-radius, radius);
+		StdDraw.clear();
+		StdDraw.picture(0, 0, "images/starfield.jpg");
+		StdDraw.show();
+		for (Planet p : planets) {
+			p.draw();
+		}
+		double time = 0;
+		while (time < t) {
+			double[] xForces = new double[planets.length];
+			double[] yForces = new double[planets.length];
+			for (int i = 0; i < planets.length; i++) {
+				xForces[i] = planets[i].calcNetForceExertedByX(planets);
+				yForces[i] = planets[i].calcNetForceExertedByY(planets);
+			}
+			for (int i = 0; i < planets.length; i++) {
+				planets[i].update(dT, xForces[i], yForces[i]);
+			}
+			StdDraw.picture(0, 0, "images/starfield.jpg");
+			for (Planet p : planets) {
+				p.draw();
+			}
+			StdDraw.show(10);
+		}
+		StdOut.printf("%d\n", planets.length);
+		StdOut.printf("%.2e\n", radius);
+		for (int i = 0; i < planets.length; i++) {
+			StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+   			planets[i].xxPos, planets[i].yyPos, planets[i].xxVel, planets[i].yyVel, planets[i].mass, planets[i].imgFileName);	
+		}
+	}
+
+	public static double readRadius(String filename) {
+		In in = new In(filename);
+		/* Keep looking until the file is empty. */
+		in.readInt();
+		return in.readDouble();
+	}
+
+	public static Planet[] readPlanets(String filename) {
+		In in = new In(filename);
+		int num = in.readInt();
+		in.readDouble();
+		Planet[] planets = new Planet[num];
+		for (int i=0; i<num; i++) {
+			planets[i] = new Planet (in.readDouble(), in.readDouble(), in.readDouble(), 
+									in.readDouble(), in.readDouble(), in.readString());
+		}
+		return planets;
+	}
+} 
--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,2 +1,81 @@
 public class Planet {
+	public double xxPos;
+	public double yyPos;
+	public double xxVel;
+	public double yyVel;
+	public double mass;
+	public String imgFileName;
+
+	public Planet(Planet p) {
+		xxPos = p.xxPos;
+		yyPos = p.yyPos;
+		xxVel = p.xxVel;
+		yyVel = p.yyVel;
+		mass = p.mass;
+		imgFileName = p.imgFileName;
+	}
+
+	public Planet(double xP, double yP, double xV,
+              double yV, double m, String img) {
+		xxPos = xP;
+		yyPos = yP;
+		xxVel = xV;
+		yyVel = yV;
+		mass = m;
+		imgFileName = img;
+	}
+
+	public double calcDistance(Planet p) {
+		double xDist = p.xxPos - xxPos;
+		double yDist = p.yyPos - yyPos;
+		return Math.sqrt(xDist*xDist + yDist*yDist);
+	}
+
+	public double calcForceExertedBy(Planet p) {
+		double dist = calcDistance(p);
+		double g = 6.67 /100000000000.0;
+		return (g * mass * p.mass) / (dist*dist);
+	}
+
+	public double calcForceExertedByX (Planet p) {
+		return calcForceExertedBy(p) * (p.xxPos - xxPos) / calcDistance(p);
+	}
+
+	public double calcForceExertedByY (Planet p) {
+		return calcForceExertedBy(p) * (p.yyPos - yyPos) / calcDistance(p);
+	}
+
+	public double calcNetForceExertedByX (Planet[] planets) {
+		double f = 0;
+		for (Planet p : planets) {
+			if (! this.equals(p)) {
+				f += calcForceExertedByX(p);
+			}
+		}
+		return f;
+	}
+
+	public double calcNetForceExertedByY (Planet[] planets) {
+		double f = 0;
+		for (Planet p : planets) {
+			if (! this.equals(p)) {
+				f += calcForceExertedByY(p);
+			}
+		}
+		return f;
+	}
+
+	public void update (double dT, double fX, double fY) {
+		double accX = fX / mass;
+		double accY = fY / mass;
+		xxVel += accX*dT;
+		yyVel += accY*dT;
+		xxPos += xxVel*dT;
+		yyPos += yyVel*dT;
+	}
+
+	public void draw() {
+		StdDraw.picture(xxPos, yyPos, "images/"+imgFileName);
+		//StdDraw.show(10);
+	}
 }
