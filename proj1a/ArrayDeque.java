ab9d5afa9fb9ce7c29a8db23915ac954126c76ff
Anna  Brewer <Anna@calvisitor-10-105-138-203.calvisitor.1918.berkeley.edu>
2017-02-03 15:01:23 -0800

    Take 2

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,114 +1,112 @@
 public class ArrayDeque<Item> {
-	int size;
-	Item[] items;
-	int nextFirst;
-	int nextLast;
+    private int size;
+    private Item[] items;
+    private int nextFirst;
+    private int nextLast;
 
-	public ArrayDeque() {
-		items = (Item[]) new Object[8];
-		nextFirst = 3;
-		nextLast = 4;
-	}
+    public ArrayDeque() {
+        items = (Item[]) new Object[8];
+        nextFirst = 3;
+        nextLast = 4;
+        size = 0;
+    }
 
-	public void addFirst(Item x) { 
-		if (size == items.length) {
-				resize(size*2);
-		}
-		size += 1;
-		items[nextFirst] = x;
-		//System.out.println(x);
-		//System.out.println(nextFirst);
-		if (nextFirst == 0) {
-			nextFirst = items.length-1;
-		}
-		else {
-			nextFirst -= 1;
-		}		
-		//System.out.println(size);
-	}
+    public void addFirst(Item x) { 
+        if (size == items.length) {
+            resize(size * 2);
+        }
+        size += 1;
+        items[nextFirst] = x;
+        //System.out.println(x);
+        //System.out.println(nextFirst);
+        if (nextFirst == 0) {
+            nextFirst = items.length - 1;
+        } else {
+            nextFirst -= 1;
+        }
+        //System.out.println(size);
+    }
 
-	public void addLast(Item x) {
-		if (size == items.length) {
-			resize(size*2);
-		}
-		size += 1;
-		items[nextLast] = x;
-		//System.out.println(x);
-		//System.out.println(nextLast);
-		if (nextLast == items.length-1) {
-			nextLast = 0;
-		}
-		else {
-			nextLast += 1;
-		}
-		
-		//System.out.println(size);
-	}
+    public void addLast(Item x) {
+        if (size == items.length) {
+            resize(size * 2);
+        }
+        size += 1;
+        items[nextLast] = x;
+        //System.out.println(x);
+        //System.out.println(nextLast);
+        nextLast = (nextLast+1)%items.length;
+        /*if (nextLast == items.length - 1) {
+            nextLast = 0;
+        }
+        else {
+            nextLast += 1;
+        }*/
+        
+        //System.out.println(size);
+    }
 
-	public void resize(int newSize) {
-		//System.out.println(size);
-		//System.out.println(nextFirst);
-		//System.out.println(nextLast);
-		//System.out.println("resize");
-		Item[] a = (Item[]) new Object[newSize]; 
-		int index = nextLast;
-		int counter = 0;
-		while (counter < size) {
-			a[counter] = items[index];
-			counter += 1;
-			index += 1;
-			if (index == size) {
-				index = 0;
-			}
-		}
-		//System.arraycopy(items, nextFirst+1, a, 0, items.length-nextFirst);
-		//System.arraycopy(items, 0, a, items.length-nextFirst, nextFirst-nextLast);
-		nextLast = size;
-		nextFirst = a.length;
-		items = a;
-	}
+    private void resize(int newSize) {
+        //System.out.println(size);
+        //System.out.println(nextFirst);
+        //System.out.println(nextLast);
+        //System.out.println("resize");
+        Item[] a = (Item[]) new Object[newSize]; 
+        int index = nextLast;
+        int counter = 0;
+        while (counter < size) {
+            a[counter] = items[index];
+            counter += 1;
+            index += 1;
+            if (index == size) {
+                index = 0;
+            }
+        }
+        //System.arraycopy(items, nextFirst+1, a, 0, items.length-nextFirst);
+        //System.arraycopy(items, 0, a, items.length-nextFirst, nextFirst-nextLast);
+        nextLast = size;
+        nextFirst = a.length;
+        items = a;
+    }
 
-	public boolean isEmpty() {
-		if (size == 0)
-			return true;
-		else
-			return false;
-	}
+    public boolean isEmpty() {
+        return size == 0;
+    }
 
-	public int size() {
-		return size;
-	}
+    public int size() {
+        return size;
+    }
 
-	public void printDeque() {
-		for (Item i : items) {
-			System.out.print(i + " ");
-		}
-		System.out.println(nextFirst);
-		System.out.println(nextLast);
-		/*int index = nextLast;
-		while (index != nextFirst) {
-			index += 1;
-			if (index == size) {
-				index = 0;
-			}
-			System.out.print(items[index]);
-		}*/
-	}
+    public void printDeque() {
+        for (Item i : items) {
+            System.out.print(i + " ");
+        }
+        System.out.println(nextFirst);
+        System.out.println(nextLast);
+        /*int intndex = nextLast;
+        while (index != nextFirst) {
+            index += 1;
+            if (index == size) {
+                index = 0;
+            }
+            System.out.print(items[index]);
+        }*/
+    }
 
-	public Item removeFirst() {
-		Item x = get(nextFirst);
-		size -= 1;
-		return x;
-	}
+    public Item removeFirst() {
+        Item x = get(0);
+        size -= 1;
+        return x;
+    }
 
-	public Item removeLast() {
-		Item x = get(nextLast);
-		size -= 1;
-		return x;
-		//do not need to actually set last item to 0
-	}
+    public Item removeLast() {
+        Item x = get(size-1);
+        size -= 1;
+        return x;
+        //do not need to actually set last item to 0
+    }
 
-	public Item get(int index) {
-		return items[index];
-	}
+    public Item get(int index) {
+        return items[(nextFirst+index+1)%items.length];
+    }
 }
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,107 +1,125 @@
-public class LinkedListDeque<Item>{
-	private int size;
-	private StuffNode sentinel;
+public class LinkedListDeque<Item> {
+    private int size;
+    private StuffNode sentinel;
 
-	public LinkedListDeque() {
-		sentinel = new StuffNode (null, null, null);
-		size = 0;
-	}
-	
-	private class StuffNode {
-		public Item item;
-		public StuffNode next;
-		public StuffNode prev;
+    public LinkedListDeque() {
+        sentinel = new StuffNode(null, null, null);
+        size = 0;
+    }
+    
+    private class StuffNode {
+        private Item item;
+        private StuffNode next;
+        private StuffNode prev;
 
-		public StuffNode(StuffNode p, Item i, StuffNode n) {
-			prev = p;
-			item = i;
-			next = n;
-		}
-	}
+        private StuffNode(StuffNode p, Item i, StuffNode n) {
+            prev = p;
+            item = i;
+            next = n;
+        }
 
-	public void addFirst(Item x) { 
-		sentinel.next = new StuffNode(sentinel, x, sentinel.next);
-		size += 1;
-	}
+        public StuffNode getPrev() {
+        	return prev;
+        }
 
-	public void addLast(Item x) {
-		sentinel.prev = new StuffNode(sentinel.prev, x, sentinel);
-		size += 1;
-		/*StuffNode p = sentinel;
-		while (p.next != null) {
-			p = p.next;
-		}
-		p.next = new StuffNode(x, null);*/
-		//make it double ended!!
-	}
+        public Item getItem() {
+        	return item;
+        }
 
-	public boolean isEmpty() {
-		if (size == 0) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
+        public StuffNode getNext() {
+        	return next;
+        }
 
-	public int size() {
-		return size;
-	}
+        public void setPrev(StuffNode i) {
+        	prev = i;
+        }
 
-	public void printDeque() {
-		StuffNode p = sentinel;
-		//idk why this works
-		while (p.next != null) {
-			System.out.println(p.item);
-			p = p.next;
-		}
-	}
+        public void setItem(Item i) {
+        	item = i;
+        }
 
-	public Item removeFirst() {
-		Item temp = sentinel.next.item;
-		sentinel.next = sentinel.next.next;
-		size -= 1;
-		return temp;
-	}
+        public void setNext(StuffNode i) {
+        	next = i;
+        }
+    }
 
-	public Item removeLast() {
-		Item temp = sentinel.prev.item;
-		sentinel.prev = sentinel.prev.prev;
-		size -= 1;
-		return temp;
-		/*StuffNode p = sentinel;
-		int counter = 2;
-		while (counter < size) {
-			p = p.next;
-			counter += 1;
-		}
-		StuffNode temp = p.next;
-		p.next = sentinel;
-		size -= 1;
-		return temp.item;*/
-	}
+    public void addFirst(Item x) { 
+        sentinel.setNext(new StuffNode(sentinel, x, sentinel.next));
+        size += 1;
+    }
 
-	public Item get(int index) {
-		StuffNode p = sentinel;
-		while (index > 0) {
-			p = p.next;
-			index -= 1;
-		}
-		return p.item;
-	}
+    public void addLast(Item x) {
+        sentinel.setPrev(new StuffNode(sentinel.prev, x, sentinel));
+        size += 1;
+        /*StuffNode p = sentinel;
+        while (p.next != null) {
+            p = p.next;
+        }
+        p.next = new StuffNode(x, null);*/
+        //make it double ended!!
+    }
 
-	public Item getRecursive(int index) {
-		StuffNode p = sentinel;
-		return getRecursiveHelper(p, index);
-	}
+    public boolean isEmpty() {
+        return size == 0;
+    }
 
-	public Item getRecursiveHelper (StuffNode p, int index) {
-		if (index == 0) {
-			return p.item;
-		}
-		else {
-			return getRecursiveHelper(p.next, index-1);
-		}
-	}
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        StuffNode p = sentinel;
+        //idk why this works
+        while (p.getNext() != null) {
+            System.out.println(p.item);
+            p = p.getNext();
+        }
+    }
+
+    public Item removeFirst() {
+        Item temp = sentinel.getNext().getItem();
+        sentinel.setNext(sentinel.getNext().getNext());
+        size -= 1;
+        return temp;
+    }
+
+    public Item removeLast() {
+        Item temp = sentinel.getPrev().getItem();
+        sentinel.setPrev(sentinel.getPrev().getPrev());
+        size -= 1;
+        return temp;
+        /*StuffNode p = sentinel;
+        int counter = 2;
+        while (counter < size) {
+            p = p.next;
+            counter += 1;
+        }
+        StuffNode temp = p.next;
+        p.next = sentinel;
+        size -= 1;
+        return temp.item;*/
+    }
+
+    public Item get(int index) {
+        StuffNode p = sentinel;
+        while (index > 0) {
+            p = p.getNext();
+            index -= 1;
+        }
+        return p.item;
+    }
+
+    public Item getRecursive(int index) {
+        StuffNode p = sentinel;
+        return getRecursiveHelper(p, index);
+    }
+
+    private Item getRecursiveHelper(StuffNode p, int index) {
+        if (index == 0) {
+            return p.getItem();
+        }
+        else {
+            return getRecursiveHelper(p.next, index - 1);
+        }
+    }
 }
-
