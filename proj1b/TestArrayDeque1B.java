1b01e1746dfbc943bcbe4aab328207791289b92a
Anna  Brewer <Anna@calvisitor-10-105-137-190.calvisitor.1918.berkeley.edu>
2017-02-10 02:29:08 -0800

    :^)

--- proj1b/TestArrayDeque1B.java
+++ proj1b/TestArrayDeque1B.java
@@ -6,56 +6,59 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestArrayDeque1B {
-
+    String message = "";
+    int length = 0;
+    StudentArrayDeque<Integer> student = new StudentArrayDeque<Integer>();
+    ArrayDequeSolution<Integer> answer = new ArrayDequeSolution<Integer>();
     @Test
     public void testArrayDeque() {
-        StudentArrayDeque<Integer> student = new StudentArrayDeque<Integer>();
-        ArrayDequeSolution<Integer> answer = new ArrayDequeSolution<Integer>();
-        int length = 0;
-        String message = "";
-        for (int  i = 0; i < 1000; i++) {
-            double method = StdRandom.uniform();
-            int number = StdRandom.uniform(9);
-            if (method < .5) {
-                message += ("addFirst(" + number + ")");
-                student.addFirst(number);
-                answer.addFirst(number);
-                message += "\n";
-            } else {
-                message += ("addLast(" + number + ")");
-                student.addLast(number);
-                answer.addLast(number);
-                message += "\n";
+        for (int i = 0; i < 1000; i++) {
+            int number = StdRandom.uniform(4);
+            if (0 <= number && number < 1) {
+                addFirst();
+            } else if (1 <= number && number < 2) {
+                addLast();
+            } else if (2 <= number && number < 3) {
+                removeFirst();
+            } else if (3 <= number && number < 4) {
+                removeLast();
             }
         }
-        for (int  i = 0; i < 999; i++) {
-            double method = StdRandom.uniform();
-            if (method < .5) {
-                message += "removeFirst()";
-                assertEquals(message, answer.removeFirst(), student.removeFirst());
-                message += "\n";
+    }
 
-            } else {
-                message += "removeLast()";
-                //int m = student.removeLast();
-                //int n = answer.removeLast();
-                assertEquals(message, answer.removeFirst(), student.removeFirst());
-                message += "\n";
+    private void addFirst() {
+        int number = StdRandom.uniform(9);
+        message += ("addFirst(" + number + ")");
+        student.addFirst(number);
+        answer.addFirst(number);
+        message += "\n";
+        length += 1;
+    }
 
-            }
+    private void addLast() {
+        int number = StdRandom.uniform(9);
+        message += ("addLast(" + number + ")");
+        student.addLast(number);
+        answer.addLast(number);
+        message += "\n";
+        length += 1;
+    }
+
+    private void removeFirst() {
+        if (length > 0) {
+            message += "removeFirst()";
+            assertEquals(message, answer.removeFirst(), student.removeFirst());
+            message += "\n";
+            length -= 1;
         }
-        //System.out.print(message);
-        /*for (int  i = 0; i < 500; i++) {
-            if (method == 0) {
-                message += "size()";
-                assertEquals(message, student.size(), answer.size());
-                message += "\n";
-            } else if (method == 1) {
-                int index = StdRandom.uniform(length);
-                message += "get()";
-                assertEquals(message, student.get(index), answer.get(index));
-                message += "\n";
-            }
-        }*/
+    }
+
+    private void removeLast() {
+        if (length > 0) {
+            message += "removeLast()";
+            assertEquals(message, answer.removeFirst(), student.removeFirst());
+            message += "\n";
+            length -= 1;
+        }
     }
 }
