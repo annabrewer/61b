f086877c18692de87df20bdd19fed3594399598b
Anna  Brewer <Anna@calvisitor-10-105-138-57.calvisitor.1918.berkeley.edu>
2017-02-09 19:52:35 -0800

    :^)

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -4,7 +4,9 @@
 public class Palindrome {
 
     public static void main (String[] args) {
-        System.out.println(isPalindrome("toohottohoot"));
+        OffByOne obo = new OffByOne();
+        //System.out.println(isPalindrome("detrude", obo));
+        //System.out.println(isPalindrome("toohottohoot"));
     }
 
     public static Deque<Character> wordToDeque(String word) {
@@ -17,23 +19,26 @@ public class Palindrome {
 
     public static boolean isPalindrome(String word) {
         if (word.length() <= 1) {
-            System.out.println(word);
+            //System.out.println(word);
             return true;
         } else if (word.charAt(word.length() - 1) == word.charAt(0)) {
-            System.out.println(word);
+            //System.out.println(word);
             return isPalindrome(word.substring(1, word.length() - 1));
         } else {
-            System.out.println(word);
+            //System.out.println(word);
             return false;
         }
     }
 
     public static boolean isPalindrome(String word, CharacterComparator cc) {
         if (word.length() <= 1) {
+            //System.out.println(word);
             return true;
         } else if (cc.equalChars(word.charAt(word.length() - 1), word.charAt(0))) {
-            return isPalindrome(word.substring(1, word.length() - 1));
+            //System.out.println(word);
+            return isPalindrome(word.substring(1, word.length() - 1), cc);
         } else {
+            //System.out.println(word);
             return false;
         }
     }
