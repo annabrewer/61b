81ea779e2db6600ef247ab7ad9bdad9bba2a2e34
Anna  Brewer <Anna@calvisitor-10-105-136-85.calvisitor.1918.berkeley.edu>
2017-02-17 13:49:08 -0800

    lab 5

--- lab2/Intlist/IntListTest.java
+++ lab2/Intlist/IntListTest.java
@@ -66,4 +66,14 @@ public class IntListTest {
         assertEquals(IntList.list(1, 2, 3), A);
     }
 
+    @Test
+    public void testReverse() {
+        IntList A = IntList.list(1, 2, 3);
+        IntList B = IntList.reverse(A);
+        assertNotEquals(A, B);
+        IntList C = IntList.list(null);
+        IntList D = IntList.reverse(C);
+        assertEquals(D, null);
+    }
+
 }
