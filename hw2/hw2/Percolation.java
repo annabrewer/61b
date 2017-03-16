e99961e042d2f223d3cfb8a880e4b534bd6b6478
Anna <annabrewer@berkeley.edu>
2017-03-15 20:20:46 -0700

    done maybe

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -1,7 +1,103 @@
 package hw2;                       
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+import java.util.ArrayList;
 
 public class Percolation {
 
+    public ArrayList<Integer> openSites;
+    public WeightedQuickUnionUF union;
+    public WeightedQuickUnionUF unionNB; //no backwash
+    public static int size;
+    public static int length;
+    public int virtualTop;
+    public int virtualBottom;
+
+
+    public Percolation(int N){
+        size = N*N;
+        length = N;
+        virtualTop = size;
+        virtualBottom = size + 1;
+        openSites = new ArrayList<Integer>();
+        union = new WeightedQuickUnionUF(size + 2);
+        unionNB = new WeightedQuickUnionUF(size+1);
+        for (int i = 0; i < N; i++){
+            union.union(i, virtualTop); //top row
+            unionNB.union(i, virtualTop); //top row
+            union.union(size - (i + 1), virtualBottom); //bottom row
+        }
+    }                // create N-by-N grid, with all sites initially blocked
+    public void open(int row, int col){
+        int site = xyTo1D(row, col);
+        if(!openSites.contains(site)){
+            openSites.add(site);
+            ArrayList<Integer> surrounding = new ArrayList<Integer>();
+            if(row != 0){
+                surrounding.add(xyTo1D(row - 1, col));
+            }
+            if(row != length - 1){
+                surrounding.add(xyTo1D(row + 1, col));
+            }
+            if(col != 0){
+                surrounding.add(xyTo1D(row, col - 1));
+            }
+            if(col != length-1){
+                surrounding.add(xyTo1D(row, col + 1));
+            }
+            for(int i : surrounding){
+                if (openSites.contains(i)){ //aka isOpen with one argument
+                    union.union(i, site);
+                    unionNB.union(i, site);
+                }
+            }
+        }
+    }       // open the site (row, col) if it is not open already
+    public boolean isOpen(int row, int col){
+        int site = xyTo1D(row, col);
+        return openSites.contains(site);
+    }  // is the site (row, col) open?
+
+    public boolean isFull(int row, int col){
+        int site = xyTo1D(row, col);
+        return isOpen(row, col) && unionNB.connected(virtualTop, site);
+    }  // is the site (row, col) full?
+
+    public int numberOfOpenSites(){
+        return openSites.size();
+    }           // number of open sites
+
+    public boolean percolates(){
+        return union.connected(virtualTop, virtualBottom);
+    }              // does the system percolate?
+
+    public static void main(String[] args){
+        //length = 5;
+        //System.out.print(xyTo1D(2, 4)); //should be 14
+        //test whether percolation works properly
+        Percolation p = new Percolation(5);
+        p.open(0, 4);
+        if (p.percolates()) {
+            System.out.print("oh no");
+        }
+        else{
+            System.out.print("hell yeah boys");
+        }
+        /*for (int i = 1; i < p.length; i++){
+            p.open(i, 4);
+        }
+        if (p.percolates()) {
+            System.out.print("hell yeah");
+        }
+        else{
+            System.out.print("oh no");
+        }*/
+    }
+
+    //helper
+    public static int xyTo1D(int r, int c){
+        int result = length * (r); //not r-1 because rows start at 0
+        result += (c); //not c+1 because cols start at 0
+        return result;
+    }
 }                       
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,63 @@
-package hw2;                       
+package hw2;
+
+import java.util.ArrayList;
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
 
 public class PercolationStats {
 
+    public int[] thresholds;
+
+    public PercolationStats(int N, int T){
+        Percolation p = new Percolation(N);
+        thresholds = new int[T];
+        //ArrayList<Integer> thresh = new ArrayList<Integer>();
+        for(int i = 0; i < T; i++){
+            int thresh = 0;
+            while(!p.percolates()){
+                thresh += 1;
+                int r = (int)StdRandom.uniform()*N;
+                int c = (int)StdRandom.uniform()*N;
+                p.open(r, c);
+            }
+            thresholds[i] = thresh;
+        }
+    }   // perform T independent experiments on an N-by-N grid
+
+    public double mean(){
+        return StdStats.mean(thresholds);
+    }                    // sample mean of percolation threshold
+
+    public double stddev(){
+        return StdStats.stddev(thresholds);
+    }                  // sample standard deviation of percolation threshold
+
+    public double confidenceLow(){
+        double threshEstimate = mean();
+        double sharpness = stddev();
+        double T = thresholds.length;
+        return threshEstimate - ((1.96 * sharpness) / Math.sqrt(T));
+
+    }           // low  endpoint of 95% confidence interval
+
+    public double confidenceHigh(){
+        double threshEstimate = mean();
+        double sharpness = stddev();
+        double T = thresholds.length;
+        return threshEstimate + ((1.96 * sharpness) / Math.sqrt(T));
+    }
+
+    //helper
+    /*public double thingamajig() { //1.96o/sqrt(T)
+        double sum = 0;
+        double threshEstimate = mean();
+        double sharpness = stddev();
+        double T = thresholds.length;
+        for (int i = 0; i < T; i++){
+            double j = (thresholds[i] + threshEstimate);
+            sum += j * j;
+        }
+        double o = sum / (T - 1);
+        return (1.96 * sharpness) / Math.sqrt(T);
+    }*/
 }                       
